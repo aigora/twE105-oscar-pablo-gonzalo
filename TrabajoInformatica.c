2318580ab10eb39c0036d1e3d014bfea1419e8ef
pablofornieleslorite <pablo.fornieles.lorite@alumnos.upm.es>
2019-05-19 11:41:49 +0200

    Update TrabajoInformatica.c

--- TrabajoInformatica.c
+++ TrabajoInformatica.c
@@ -271,7 +271,6 @@ void modificar_precio(Producto p[], int dim){
 		
 		printf("Precio modificado: \n");
 		printf("%s %f", p[i].codigo, p[i].precio);
-		sleep(3);
 		f = fopen("Almacen.txt", "w");
 		
 		if(f == NULL)
