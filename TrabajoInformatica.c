47ebdff88c468b913d997a3b427fa34e782c37c5
oscardmpj <36692749+oscardmpj@users.noreply.github.com>
2019-05-20 10:46:07 +0200

    Update TrabajoInformatica.c

--- TrabajoInformatica.c
+++ TrabajoInformatica.c
@@ -296,8 +296,8 @@ void modificar_precio(Producto p[], int dim){
 }
 
 void pedido(Producto p[], int dim){
-	int i, cant=0, flag1, j, flag = 1, lista1[30],lista2[30], x, cont=0;
-	char cod[5], gen[10], clas[15], tip[10], marc[10];
+	int i, cant=0, j, flag = 1, lista1[30],lista2[30], x, cont=0;
+	char cod[5], gen[10], clas[15], tip[10], marc[10], flag1;
 	float prec=0;
 	FILE *f;
 	
@@ -372,10 +372,10 @@ void pedido(Producto p[], int dim){
 		else
 			printf("El producto seleccionado no esta en nuestro stock\n");
 			
-		printf("Introduzca 1 si quiere seguir añadiendo algun producto mas o cualquier otro num para salir, gracias.\n");
-		scanf("%d", &flag1);
+		printf("\nIntroduzca s si quiere seguir añadiendo algun producto mas o cualquier otro numo letra para salir, gracias.\n");
+		scanf("%c", &flag1);
 		fflush(stdin);
-	}while(flag1 == 1);	
+	}while(flag1 == 's' || flag1 == 'S');	
 	
 	printf("\nPulse intro para volver al menu\n");
 	while(getchar() != '\n');
